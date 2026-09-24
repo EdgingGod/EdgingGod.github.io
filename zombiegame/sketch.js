d1db036d863cd8f89f5a1e42d6c53b164194743d
EdgingGod <163838456+EdgingGod@users.noreply.github.com>
2026-09-24 13:03:47 -0600

    working on project

--- zombiegame/sketch.js
+++ zombiegame/sketch.js
@@ -6,19 +6,51 @@
 // - describe what you did to take this project "above and beyond"
 
 // local veriables 
-let mainplayer, x, y;
+let mainplayer, x, y, playerSpeed, zombieX, zombieY, zombieSize;
 
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke()
-  rectmode(CENTER)
-  mainplayer = await loadImage('mainplayer.png')
+  noStroke();
+  mainplayer = await loadImage('mainplayer.png');
   x = windowWidth/2;
   y = windowHeight/2;
+  zombieX = random(-windowWidth, windowWidth);
+  zombieY = random(-windowHeight, windowWidth);
+  zombieSize = random(50, 100);
+  playerSpeed = 3;
 }
 
 function draw() {
-  background(220);
-  Image(mainplayer, x, y)
+  background(0);
+  showingPlayer();
+  movingPlayer();
+  spawningZombies();
 }
+
+//functions
+function showingPlayer(){
+  imageMode(CENTER);
+  image(mainplayer, x, y, mainplayer.width/5 , mainplayer.height/5);
+}
+
+function movingPlayer(){
+  if (keyIsDown("w") || keyIsDown(UP_ARROW)) {
+    y -= playerSpeed;
+  }
+  else if (keyIsDown("s") || keyIsDown(DOWN_ARROW)) {
+    y += playerSpeed;
+  }
+  else if (keyIsDown("d") || keyIsDown(RIGHT_ARROW)) {
+    x += playerSpeed;
+  }
+  else if (keyIsDown("a") || keyIsDown(LEFT_ARROW)) {
+    x -= playerSpeed;
+  }
+}
+
+function spawningZombies(){
+  fill("green");
+  square(zombieX, zombieY, zombieSize);
+}
+
